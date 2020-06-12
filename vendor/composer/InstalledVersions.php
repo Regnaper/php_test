f19f44448eb7cf3cb7ee83d27b09b3050551ba54
regnaper <iv_vod@mail.ru>
2020-06-12 19:30:03 +0300

    Add Procfile

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -20,7 +20,7 @@ private static $installed = array (
     array (
       0 => '9999999-dev',
     ),
-    'reference' => '850b7098e51d71ff753ca91911de79728a230022',
+    'reference' => 'd0e29df4f4e8cfb17712918cae126b8ccfad2b86',
     'name' => '__root__',
   ),
   'versions' => 
@@ -33,7 +33,17 @@ private static $installed = array (
       array (
         0 => '9999999-dev',
       ),
-      'reference' => '850b7098e51d71ff753ca91911de79728a230022',
+      'reference' => 'd0e29df4f4e8cfb17712918cae126b8ccfad2b86',
+    ),
+    'heroku/heroku-buildpack-php' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '6c6cc209a95c65f01d4715540459d5d673be19dc',
     ),
   ),
 );
