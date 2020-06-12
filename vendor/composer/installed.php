d0e29df4f4e8cfb17712918cae126b8ccfad2b86
regnaper <iv_vod@mail.ru>
2020-06-12 19:01:35 +0300

    Add composer other files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,26 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+      0 => '9999999-dev',
+    ),
+    'reference' => '850b7098e51d71ff753ca91911de79728a230022',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '850b7098e51d71ff753ca91911de79728a230022',
+    ),
+  ),
+);
