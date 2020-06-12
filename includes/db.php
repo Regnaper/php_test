2c62ecfcd0a4f3366d95aa94133485f82988cf53
regnaper <iv_vod@mail.ru>
2020-06-12 18:14:11 +0300

    Add db credentials

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,19 @@
 <?php
 
-    $connection = mysqli_connect('zgt', 'root', '', 'zgt_db');
+    if ($_SERVER['SERVER_NAME'] == "php-math-test.herokuapp.com") {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $dbname = substr($url["path"], 1);
+    } else {
+        $host = 'zgt';
+        $dbname = 'zgt_db';
+        $username = 'root';
+        $password = '';
+    }
+
+    $connection = mysqli_connect($host, $username, $password, $dbname);
     if (!$connection) {
         echo mysqli_connect_error();
         exit();
